1301da67df131d97f47d38da2c4b8ae62c867e68
Hadakk17 <adakli46@gmail.com>
2023-05-18 13:13:55 +0300

    dosyalar

--- swproject/src/Components/navbar/navbar.js
+++ swproject/src/Components/navbar/navbar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome,faUsers,faChartLine,faUtensils,faBook } from '@fortawesome/free-solid-svg-icons';
+import { faHome,faUsers,faBook } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css';
 
 function Navbarx() {
@@ -11,28 +11,18 @@ function Navbarx() {
       <Container>
         <Navbar.Brand style={{fontFamily:"Times New Roman",fontSize:"18px",paddingRight:"100px"}} href="#home">
           <FontAwesomeIcon icon={faHome} style={{marginRight: "10px"}} />
-          Ana Sayfa
+          Yemek Tarifleri
         </Navbar.Brand>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
              <Nav.Link style={{fontFamily:"Times New Roman",fontSize:"18px",paddingRight:"100px"}} href="#home">
                 <FontAwesomeIcon icon={faBook} style={{marginRight: "10px"}} />
-                Tarifler
-            </Nav.Link>
-            
-            <Nav.Link style={{fontFamily:"Times New Roman",fontSize:"18px",paddingRight:"100px"}} href="#home">
-                <FontAwesomeIcon icon={faUtensils} style={{marginRight: "10px"}} />
-                Yemekler
-            </Nav.Link>
-            
-            <Nav.Link style={{fontFamily:"Times New Roman",fontSize:"18px",paddingRight:"100px"}} href="#home">
-                <FontAwesomeIcon icon={faChartLine} style={{marginRight: "10px"}} />
-                Kalori
+                İçindekiler
             </Nav.Link>
             <Nav.Link style={{fontFamily:"Times New Roman",fontSize:"18px",paddingRight:"100px"}} href="#home">
                 <FontAwesomeIcon icon={faUsers} style={{marginRight: "10px"}} />
-                hakkımızda
+                Profile
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
