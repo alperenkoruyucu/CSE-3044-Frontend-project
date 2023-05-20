f3a4b9413d0bdbc82802c4c8f5d0c4f9fd4a6597
alperenkoruyucu <alperenkoruyucu@hotmail.com>
2023-05-20 17:54:50 +0300

    recepies improvement

--- swproject/src/pages/Recipes/Recipe.js
+++ swproject/src/pages/Recipes/Recipe.js
@@ -1,50 +1,51 @@
 import './Recipe.css'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import './Recipe.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import list from './../../recipies.json';
+import { useEffect, useState } from 'react';
 
 function Recipe() {
-    return (  
-        
-        <div className='Recipe'>                       
-            
-            <div className='RecipeContainer ' style={{display:"flex", flexDirection:"column", justifyContent:"center", padding: "85px"  }}>
-                
-                <form>
+    console.log('list', list)
+    const [recipes, setRecipes] = useState([])
+    useEffect(() => {
+        setRecipes(list["Recipe"])
+    }, []);
+    console.log("=>", recipes)
+    return (
+        <div class="container" style={{ marginTop: 120 }}>
+            <div class="row">
+                <div class="col-12">
+                    <div>
                     <h1>Fit Eats</h1>
-                    <label for="inlineCheckbox1">Write Your Ingridients:</label>
+                        <label>
+                            Choose your Recipe from this list: &nbsp;
+                            <input list="recipies" class="input" name="myIngridients" />
+                        </label>
+                        <datalist id="recipies">
+                            {
+                                Object.values(recipes).map(item => <option value={item.Name} />)
+                            }
+                        </datalist>
+                    </div>
+                </div>
+                <div class="col-12 mt-2">
+                    <div>
+                        <label>
+                            Choose your Catagory from this list: &nbsp;
+                            <input list="category" class="input" name="myCategory" />
+                        </label>
+                        <datalist id="category">
+                            {
+                                Object.values(recipes).map(item => <option value={item.MainCategory} />)
+                            }
+                        </datalist>
+                    </div>
+                </div>
+            </div>
 
-                </form>                
-            </div>
-            <div class="input-group ">
-            <label>
-            Choose your Ingridients from this list: &nbsp;
-            <input list="Ingridients" class="input" name="myIngridients" />  
-            </label>   
-            <datalist id="Ingridients">
-                <option value="kek" />
-                <option value="pasta" />
-                <option value="muz" />
-                <option value="salata" />
-                <option value="havuç" />
-                <option value="patates" />   
-            </datalist>
-            </div>
-            <div class="select  ">
-                <form>
-                    <select class="form-select my-5 " aria-label="Default select example">
-                        <option selected>kategoriye göre ara</option>
-                        <option value="1">Ara sıcak</option>
-                        <option value="2">Atıştırmalık</option>
-                        <option value="3">Tatlı</option>
-                     </select>                     
-                </form>                            
-                
-           
-            </div>                           
-            
         </div>
+
+
     );
 }
 
